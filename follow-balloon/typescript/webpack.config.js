843d3b25eefad7a378a663ed76585ba5726c800b
harutete <ai.takeuchi.10@gmail.com>
2021-11-01 09:05:35 +0900

    Add: ex.typescriptの土台

---
+++ follow-balloon/typescript/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.ts',
+  output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'main.js'
+	},
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: false,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false
+            }
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+	devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    open: true
+	}
+};
