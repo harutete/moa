ac87a0019eae3926085bbd14d3a13075f75005d8
harutete <ai.takeuchi.10@gmail.com>
2021-11-03 22:59:49 +0900

    Update: 吹き出しの来るべき位置を取得するところまで実装

--- follow-balloon/typescript/src/js/main.ts
+++ follow-balloon/typescript/src/js/main.ts
@@ -1,3 +1,23 @@
 import '../sass/style.scss';
 
-console.log('hello')
+const followComment = () => {
+  const pickupComment = document.querySelector('.js-pickupComment') as HTMLDivElement
+  const navigationList = document.querySelector('.js-navList')
+  const targetNavigationItem = document.querySelector('.js-currentItem')
+
+  // 追従するメニューまたはコメントがない場合は処理を終了させる
+  if (!pickupComment || !targetNavigationItem) {
+    return
+  }
+
+  const navigationListPosition = navigationList.getBoundingClientRect().left
+  const navigationItemPosition = targetNavigationItem.getBoundingClientRect().left
+  const targetItemPosition = navigationItemPosition - navigationListPosition
+
+  // TODO 擬似要素はJSで制御できないのでスタイルシートを上書きするようにする
+  // pickupComment.style.left = `${targetItemPosition}px`
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  followComment()
+})
