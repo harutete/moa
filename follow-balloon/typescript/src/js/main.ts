591889637158756767e0280dc779bcdab614c25a
harutete <ai.takeuchi.10@gmail.com>
2021-11-05 23:34:40 +0900

    Update: スクロールの最大・最小値を設定する

--- follow-balloon/typescript/src/js/main.ts
+++ follow-balloon/typescript/src/js/main.ts
@@ -13,13 +13,29 @@ const followComment = () => {
   }
 
   const followBallon = () => {
+    // 左側の矢印の閾値　（角丸部分を考慮して4pxに設定）
+    const MIN_THRESHOLD_POSITION = 4
+    // 吹き出しの矢印の幅
+    const BALLON_ARROW_WIDTH = 16
+    const pickupCommentWidth = pickupComment.offsetWidth
     const navigationListPosition = navigationList.getBoundingClientRect().left
     const navigationItemPosition = targetNavigationItem.getBoundingClientRect().left
     const targetItemPosition = navigationItemPosition - navigationListPosition
+    const calcTargetItemPosition = () => {
+      if (targetItemPosition < MIN_THRESHOLD_POSITION) {
+        return MIN_THRESHOLD_POSITION
+      }
+      if (targetItemPosition > pickupCommentWidth - MIN_THRESHOLD_POSITION) {
+        console.log(pickupCommentWidth - MIN_THRESHOLD_POSITION)
+        return pickupCommentWidth - (MIN_THRESHOLD_POSITION + BALLON_ARROW_WIDTH)
+      }
+
+      return targetItemPosition
+    }
 
     sheet.insertRule(
       `.js-pickupComment::before {
-        left: ${targetItemPosition}px
+        left: ${calcTargetItemPosition()}px
       }`,
       sheet.cssRules.length
     )
