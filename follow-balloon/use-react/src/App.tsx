5ce18a69f7006b14a958a8f09c27f36f52730058
harutete <ai.takeuchi.10@gmail.com>
2021-11-18 21:15:04 +0900

    Update: 描画時にcurrenntItemの座標を取得する

--- follow-balloon/use-react/src/App.tsx
+++ follow-balloon/use-react/src/App.tsx
@@ -1,55 +1,64 @@
 import type React from 'react';
-import { useRef, createRef, LegacyRef } from 'react';
+import { useState, useEffect, useRef, createRef, LegacyRef } from 'react';
 import './App.scss';
 
+const MENU_LIST = [
+  {
+    title: 'menu01',
+    isCurrent: false
+  },
+  {
+    title: 'menu02',
+    isCurrent: false
+  },
+  {
+    title: 'menu03',
+    isCurrent: false
+  },
+  {
+    title: 'menu04',
+    isCurrent: true
+  },
+  {
+    title: 'menu05',
+    isCurrent: false
+  },
+  {
+    title: 'menu06',
+    isCurrent: false
+  },
+  {
+    title: 'menu07',
+    isCurrent: false
+  },
+  {
+    title: 'menu08',
+    isCurrent: false
+  },
+  {
+    title: 'menu09',
+    isCurrent: false
+  },
+]
+
 const App: React.FC = () => {
-  const calcArrowPosition = 0
-  const MENU_LIST = [
-    {
-      title: 'menu01',
-      isCurrent: false
-    },
-    {
-      title: 'menu02',
-      isCurrent: false
-    },
-    {
-      title: 'menu03',
-      isCurrent: false
-    },
-    {
-      title: 'menu04',
-      isCurrent: true
-    },
-    {
-      title: 'menu05',
-      isCurrent: false
-    },
-    {
-      title: 'menu06',
-      isCurrent: false
-    },
-    {
-      title: 'menu07',
-      isCurrent: false
-    },
-    {
-      title: 'menu08',
-      isCurrent: false
-    },
-    {
-      title: 'menu09',
-      isCurrent: false
-    },
-  ]
   const menuItems = useRef(MENU_LIST.map(() => createRef<HTMLLIElement>()))
+  const [arrowPosition, setArrowPosition] = useState(4)
+
+  useEffect(() => {
+    const currentItem = menuItems.current.filter((item) => /is-current/.test(String(item.current?.className)))
+    if (!currentItem.length) {
+      return
+    }
+    setArrowPosition(currentItem[0]?.current?.getBoundingClientRect().left ?? 4)
+  }, [])
 
   return (
     <div className="wrapper">
       <h1>Follow ballon</h1>
       <p className="pickupComment">
         コメント
-        <span className="pickupCommentArrow" style={{left: `${calcArrowPosition}px`}}></span>
+        <span className="pickupCommentArrow" style={{left: `${arrowPosition}px`}}></span>
       </p>
       <nav className="navWrap">
         <ul className="navList">
