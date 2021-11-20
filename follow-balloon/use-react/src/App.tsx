a4fd306dd3961cfeb37e1cad05d453f5d58686e1
harutete <ai.takeuchi.10@gmail.com>
2021-11-20 23:59:11 +0900

    Update: 座標計算のロジック修正

--- follow-balloon/use-react/src/App.tsx
+++ follow-balloon/use-react/src/App.tsx
@@ -44,18 +44,27 @@ const MENU_LIST = [
 const App: React.FC = () => {
   const wrapperRef = useRef<HTMLDivElement>(null)
   const menuItemsRef = useRef(MENU_LIST.map(() => createRef<HTMLLIElement>()))
-  const [wrapperPosition, setWrapperPosition] = useState(0)
+  const [currentItem, setCurrentItem] = useState<HTMLLIElement | null>(null)
+  const [wrapperPosition, setWrapperPosition] = useState<DOMRect | null>(null)
   const [arrowPosition, setArrowPosition] = useState(4)
+  const onScrollNavi = () => {
+    const calcItemPosition = currentItem?.getBoundingClientRect().left
+    if (!wrapperPosition?.left || !calcItemPosition) {
+      return 4
+    }
+    setArrowPosition(calcItemPosition === 4 ? 4 : calcItemPosition - wrapperPosition?.left)
+  }
 
   useEffect(() => {
-    const currentItem = menuItemsRef.current.filter((item) => /is-current/.test(String(item.current?.className)))
-    if (!wrapperRef.current || !currentItem.length) {
+    const currentItemRef = menuItemsRef.current.filter((item) => /is-current/.test(String(item.current?.className)))
+    if (!wrapperRef.current || !currentItemRef.length) {
       return
     }
-    const wrapperLeftPosition = wrapperRef.current.getBoundingClientRect().left
-    const currentItemLeftPosition = currentItem[0]?.current?.getBoundingClientRect().left ?? 4
-    setWrapperPosition(wrapperLeftPosition)
-    setArrowPosition(currentItemLeftPosition === 4 ? 4 : currentItemLeftPosition - wrapperLeftPosition)
+    const wrapperRect = wrapperRef.current.getBoundingClientRect()
+    const currentItemLeftPosition = currentItemRef[0]?.current?.getBoundingClientRect().left ?? 4
+    setCurrentItem(currentItemRef[0]?.current ?? null)
+    setWrapperPosition(wrapperRect)
+    setArrowPosition(currentItemLeftPosition === 4 ? 4 : currentItemLeftPosition - wrapperRect.left)
   }, [])
 
   return (
@@ -66,7 +75,7 @@ const App: React.FC = () => {
         <span className="pickupCommentArrow" style={{left: `${arrowPosition}px`}}></span>
       </p>
       <nav className="navWrap">
-        <ul className="navList">
+        <ul className="navList" onScroll={onScrollNavi}>
           {MENU_LIST.map((item, index) => <li key={item.title} className={item.isCurrent ? 'is-current' : undefined} ref={menuItemsRef.current[index] as unknown as LegacyRef<HTMLLIElement> | undefined}>{item.title}</li>)}
         </ul>
       </nav>
