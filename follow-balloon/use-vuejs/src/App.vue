60d89f7d577e7a218fa710ecca175082618b0230
harutete <ai.takeuchi.10@gmail.com>
2021-11-28 23:55:45 +0900

    Fix: メニュー追加

--- follow-balloon/use-vuejs/src/App.vue
+++ follow-balloon/use-vuejs/src/App.vue
@@ -7,22 +7,63 @@
     </p>
     <nav className="navWrap">
       <ul className="navList">
-        <li></li>
+        <li v-for="item in menuList" :key={item.title}>
+          {item.title}
+        </li>
       </ul>
     </nav>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  createComponent,
-  reactive,
-  SetupContext
-} from '@vue/composition-api'
+import { defineComponent } from 'vue';
 
-export default {
-  name: 'App'
-};
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const MENU_LIST = [
+      {
+        title: 'menu01',
+        isCurrent: false
+      },
+      {
+        title: 'menu02',
+        isCurrent: false
+      },
+      {
+        title: 'menu03',
+        isCurrent: false
+      },
+      {
+        title: 'menu04',
+        isCurrent: false
+      },
+      {
+        title: 'menu05',
+        isCurrent: false
+      },
+      {
+        title: 'menu06',
+        isCurrent: false
+      },
+      {
+        title: 'menu07',
+        isCurrent: false
+      },
+      {
+        title: 'menu08',
+        isCurrent: true
+      },
+      {
+        title: 'menu09',
+        isCurrent: false
+      },
+    ]
+    return {
+      menuList: MENU_LIST
+    }
+  }
+});
 </script>
 
 <style>
