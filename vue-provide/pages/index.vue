21f6163d9eb052dfb9bb66af7d4965657d9affb9
harutete <ai.takeuchi.10@gmail.com>
2021-12-21 23:31:17 +0900

    Update: 処理追加

--- vue-provide/pages/index.vue
+++ vue-provide/pages/index.vue
@@ -1,17 +1,31 @@
 <template>
-  <form @submit="addTodo">
+  <form @submit="addTodoItem">
     <div>
-      <input type="text" value="" />
+      <input type="text" v-model="todoText" />
       <button type="submit">Add todo!</button>
     </div>
   </form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { addTodo } from '../libs/todo'
 
 export default defineComponent({
   setup() {
+    const todoText = ref('')
+    const addTodoItem = () => {
+      if (!todoText.value.length) {
+        return
+      }
+
+      addTodo(todoText.value)
+    }
+
+    return {
+      addTodoItem,
+      todoText
+    }
   }
 })
 </script>
