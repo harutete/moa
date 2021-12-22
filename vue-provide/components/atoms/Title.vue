bc24705ac7769cc3841e77a791bebc66fbcb0514
harutete <ai.takeuchi.10@gmail.com>
2021-12-22 22:27:21 +0900

    Add: provide/inject

---
+++ vue-provide/components/atoms/Title.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>{{ theme }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const theme = inject('theme')
+
+    return {
+      theme
+    }
+  }
+})
+</script>
--- vue-provide/pages/index.vue
+++ vue-provide/pages/index.vue
@@ -1,23 +1,31 @@
 <template>
-  <form @submit.prevent="addTodoItem">
-    <div>
-      <input v-model="todoText" type="text" />
-      <button type="submit">Add todo!</button>
-    </div>
-  </form>
+  <div>
+    <Title />
+    <form @submit.prevent="addTodoItem">
+      <div>
+        <input v-model="todoText" type="text" />
+        <button type="submit">Add todo!</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { defineComponent, ref, provide } from '@nuxtjs/composition-api'
 import { addTodo } from '../libs/todo'
+import Title from '../components/atoms/Title.vue'
 
 export default defineComponent({
+  components: {
+    Title
+  },
   setup() {
     const todoText = ref('')
     const addTodoItem = () => {
       if (!todoText.value.length) {
         return
       }
+      provide('theme', 'black')
 
       addTodo(todoText.value)
     }
