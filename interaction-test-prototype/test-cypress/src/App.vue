65fbac0f9cb13b42850db2c704d0bc85097b3d4c
harutete <ai.takeuchi.10@gmail.com>
2023-01-05 09:20:41 +0900

    Add: CounterButton

--- interaction-test-prototype/test-cypress/src/App.vue
+++ interaction-test-prototype/test-cypress/src/App.vue
@@ -1,29 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import CounterButton from "./components/CounterButton/index.vue"
+import { ref } from 'vue'
+
+const count = ref(0)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <counter-button @click="count++" :count="count" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ interaction-test-prototype/test-cypress/src/components/CounterButton/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  count: number
+}>()
+const emit = defineEmits<{
+  (event: "click"): void
+}>()
+</script>
+
+<template>
+  <button type="button" @click="emit('click')">count is {{ props.count }}</button>
+</template>
